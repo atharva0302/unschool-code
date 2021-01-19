e85277165401d1e02a56e1e7c2df12be96798fac
atharva0302 <77654326+atharva0302@users.noreply.github.com>
2021-01-19 10:05:10 +0530

    Add files via upload

--- logical operator.cpp
+++ logical operator.cpp
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 { 
-int num1=0,num2=0,result=0;
+int num1,num2,result=0;
 printf(" enter the two no ");
 scanf("%d,%d",&num1,&num);
-result=num1+num2;
-printf("addition:%d",result);
-result=num1-num2;
-printf("subtraction:%d",result);
-result=num1*num2;
-printf("multiplication:%d",result);
-result=num1/num2;
-printf("division:%d",result);
-result=num1%num2;
-printf("remainder:%d",result); 
-result=num1++;
-printf("increment:%d",result);
-result=num2--;
-printf("decrement:%d",result);
+result=num&&num2;
+printf("and:%d",result);
+result=num1&&num2;
+printf("and:%d",result);
+result=num1&&num2;
+printf("and:%d",result);
+result=num1||num2;
+printf("or:%d",result);
+result=num1||num2;
+printf("or:%d",result); 
+result=num1||num2;
+printf("or:%d",result);
+result=num2||num1;
+printf("or:%d",result);
 return o;
 }
