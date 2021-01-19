177397e3eba2ad9beff53fb976e0b380e8cbf724
atharva0302 <77654326+atharva0302@users.noreply.github.com>
2021-01-19 10:11:11 +0530

    Add files via upload

---
+++ relational operator.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf(" \n%d",5>8);
+    printf(" \n%d",5<8);
+    printf("\n%d",5>=5);
+    printf("\n%d",5<=5);
+    printf(" \n:%d",5>5);
+    printf(" \n%d",5<5);
+    printf("\n%d",5!=5);
+      return 0;
+}
