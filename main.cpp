1026791838ede5900be543523edf510f1bf3a309
atharva0302 <77654326+atharva0302@users.noreply.github.com>
2021-01-19 09:34:47 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include<stdio.h>
+
 int main()
 {
-    printf(" \n%d",5>8);
-    printf(" \n%d",5<8);
-    printf("\n%d",5>=5);
-    printf("\n%d",5<=5);
-    printf(" \n:%d",5>5);
-    printf(" \n%d",5<5);
-    printf("\n%d",5!=5);
-      return 0;
+ printf("you are unschoolar");
+    return 0;
 }
