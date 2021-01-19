873aada5e437f1b97ad1442d4384519b3ae90f16
atharva0302 <77654326+atharva0302@users.noreply.github.com>
2021-01-19 13:00:32 +0530

    Add files via upload

---
+++ if_else1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ float percentage;
+ printf("enter the valid percentage");
+ scanf("%f",&percentage);
+ if(percentage>=45)
+ {
+     printf("king is pass");
+ }
+ else
+ {
+     printf("king is fail");
+ }
+ return 0;
+}
