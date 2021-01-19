4b6c6d156907dff5031d37e8ad0e8ad9c036fe70
atharva0302 <77654326+atharva0302@users.noreply.github.com>
2021-01-19 12:51:57 +0530

    Add files via upload

---
+++ swaping.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,temp=0;
+    printf("accept the two no num1,num2");
+    scanf("%d,%d",&num1,&num2);
+    num1=temp;
+    num1=num2;
+    num2=temp;
+    printf("num1:%d",num1);
+    printf("num2:%d",num2);
+    return 0;
+}
