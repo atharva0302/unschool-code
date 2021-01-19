35d8cade62054efda0033cc2f89fc00fce5f21b2
atharva0302 <77654326+atharva0302@users.noreply.github.com>
2021-01-19 09:43:30 +0530

    Add files via upload

---
+++ accept from user.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i=0,m=0,n=0,sum=0;
+    printf("enter the value of i,m,n");
+    scanf("%d,%d,%d",&i,&m,&n);
+    sum=i+m+n;
+    printf("enter the value sum: %d",sum);
+    return 0;
+}
+
